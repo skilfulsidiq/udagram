11bb08a80ee56fc3fb515539fb0aeb09fcf9753e
skilfulsidiq <osodiq@gmail.com>
2022-08-21 14:43:41 +0100

    hpa screen shot added

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -8,6 +8,6 @@ kubectl apply -f ./backend-feed-deployment.yaml
 
 kubectl apply -f ./backend-user-deployment.yaml
 
-kubectl apply -f ./reverseproxy-deployment.yaml
+# kubectl apply -f ./reverseproxy-deployment.yaml
 
-kubectl apply -f ./frontend-deployment.yaml
+# kubectl apply -f ./frontend-deployment.yaml
